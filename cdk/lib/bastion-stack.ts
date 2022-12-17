616bff28f0c14dc5f46941c65d9b81a3eae887ba
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2022-12-17 12:34:42 +0900

    change bastion host command

--- cdk/lib/bastion-stack.ts
+++ cdk/lib/bastion-stack.ts
@@ -3,6 +3,7 @@ import * as cdk from 'aws-cdk-lib';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as ssm from 'aws-cdk-lib/aws-ssm';
 import * as rds from 'aws-cdk-lib/aws-rds';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export interface BastionStackProps extends cdk.StackProps {
   cluster: ecs.Cluster;
@@ -17,18 +18,53 @@ export class BastionStack extends cdk.Stack {
     const dbCredential = props.dbInstance.secret!
     const activationCode = ssm.StringParameter.fromStringParameterName(this, 'ActivationCode', 'bastion-activation-code')
     const activationId = ssm.StringParameter.fromStringParameterName(this, 'ActivationId', 'bastion-activation-id')
+
+    // const taskRole = new iam.Role(this, 'TaskRole', {
+    //   assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+    //   inlinePolicies: {
+    //     'PassRole': new iam.PolicyDocument({
+    //       statements: [
+    //         new iam.PolicyStatement({
+    //           actions: ['iam:PassRole'],
+    //           resources: ['*'],
+    //           conditions: {
+    //             StringEquals: { "iam:PassedToService": "ssm.amazonaws.com" }
+    //           }
+    //         })
+    //       ],
+    //     })
+    //   }
+    // })
+    // const executionRole = new iam.Role(this, 'ExecRole', {
+    //   assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+    //   inlinePolicies: {
+    //     'AssumeRole': new iam.PolicyDocument({
+    //       statements: [
+    //         new iam.PolicyStatement({
+    //           actions: ['sts:AssumeRole'],
+    //           resources: ['*'],
+    //           principals: [new iam.ServicePrincipal('ssm.amazonaws.com')]
+    //         })
+    //       ]
+    //     })
+    //   }
+    // })
+
     const taskDef = new ecs.FargateTaskDefinition(this, 'TaskDef', {
       cpu: 256,
-      memoryLimitMiB: 512
+      memoryLimitMiB: 512,
+      // taskRole,
+      // executionRole
     })
+
     taskDef.addContainer('bastion', {
       image: props.appImage,
-      command: ['amazon-ssm-agent'],
+      command: ['sleep', '3600'],
       secrets: {
         SSM_ACTIVATION_CODE: ecs.Secret.fromSsmParameter(activationCode),
         SSM_ACTIVATION_ID: ecs.Secret.fromSsmParameter(activationId),
         DATABASE_CREDENTIALS: ecs.Secret.fromSecretsManager(dbCredential),
-      }
+      },
     })
   }
 }
