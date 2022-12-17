95c8fffac055a512b504496a7f8a137a73f36ef4
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2022-12-17 13:15:39 +0900

    automated create activation

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
 
-if [ -n "$SSM_ACTIVATION_CODE" ]; then
+if [ -n "$SSM_SERVICE_ROLE" ]; then
   # SEE: https://github.com/iselegant/aws-bastion-fargate
-  amazon-ssm-agent -register -code "$SSM_ACTIVATION_CODE" -id "$SSM_ACTIVATION_ID" -region "ap-northeast-1"
+  echo "Create SSM activation"
+  activation_parameters=$(aws ssm create-activation \
+                              --default-instance-name bastion \
+                              --description "Activation Code for Fargate Bastion" \
+                              --iam-role "$SSM_SERVICE_ROLE" \
+                              --region "ap-northeast-1")
+
+  export activation_code=`echo $activation_parameters | jq -r .ActivationCode`
+  export activation_id=`echo $activation_parameters | jq -r .ActivationId`
+
+  echo "Activate SSM Agent on Fargate Task"
+  amazon-ssm-agent -register -code "${activation_code}" -id "${activation_id}" -region 'ap-northeast-1'
+
+  echo "Delete Activation Code"
+  aws ssm delete-activation --activation-id ${activation_id}
 fi
 
 exec "$@"
--- cdk/lib/bastion-stack.ts
+++ cdk/lib/bastion-stack.ts
@@ -1,7 +1,6 @@
 import { Construct } from 'constructs';
 import * as cdk from 'aws-cdk-lib';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
-import * as ssm from 'aws-cdk-lib/aws-ssm';
 import * as rds from 'aws-cdk-lib/aws-rds';
 import * as iam from 'aws-cdk-lib/aws-iam';
 
@@ -15,57 +14,30 @@ export class BastionStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: BastionStackProps) {
     super(scope, id, props);
 
-    const dbCredential = props.dbInstance.secret!
-    const activationCode = ssm.StringParameter.fromStringParameterName(this, 'ActivationCode', 'bastion-activation-code')
-    const activationId = ssm.StringParameter.fromStringParameterName(this, 'ActivationId', 'bastion-activation-id')
-
-    // const taskRole = new iam.Role(this, 'TaskRole', {
-    //   assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
-    //   inlinePolicies: {
-    //     'PassRole': new iam.PolicyDocument({
-    //       statements: [
-    //         new iam.PolicyStatement({
-    //           actions: ['iam:PassRole'],
-    //           resources: ['*'],
-    //           conditions: {
-    //             StringEquals: { "iam:PassedToService": "ssm.amazonaws.com" }
-    //           }
-    //         })
-    //       ],
-    //     })
-    //   }
-    // })
-    // const executionRole = new iam.Role(this, 'ExecRole', {
-    //   assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
-    //   inlinePolicies: {
-    //     'AssumeRole': new iam.PolicyDocument({
-    //       statements: [
-    //         new iam.PolicyStatement({
-    //           actions: ['sts:AssumeRole'],
-    //           resources: ['*'],
-    //           principals: [new iam.ServicePrincipal('ssm.amazonaws.com')]
-    //         })
-    //       ]
-    //     })
-    //   }
-    // })
-
     const taskDef = new ecs.FargateTaskDefinition(this, 'TaskDef', {
       cpu: 256,
       memoryLimitMiB: 512,
-      // taskRole,
-      // executionRole
+    })
+    taskDef.taskRole.addToPrincipalPolicy(new iam.PolicyStatement({
+      actions: ['iam:PassRole'],
+      resources: ['*'],
+      conditions: {
+        StringEquals: { "iam:PassedToService": "ssm.amazonaws.com" }
+      }
+    }))
+
+    const dbCredential = props.dbInstance.secret!
+    const ssmServiceRole = new iam.Role(this, 'SsmServiceRole', {
+      assumedBy: new iam.ServicePrincipal('ssm.amazon.com')
     })
 
     taskDef.addContainer('bastion', {
       image: props.appImage,
-      command: ['sleep', '$DURATION'],
+      command: ['amazon-ssm-agent'],
       environment: {
-        DURATION: '3600'
+        SSM_SERVICE_ROLE: ssmServiceRole.roleName
       },
       secrets: {
-        SSM_ACTIVATION_CODE: ecs.Secret.fromSsmParameter(activationCode),
-        SSM_ACTIVATION_ID: ecs.Secret.fromSsmParameter(activationId),
         DATABASE_CREDENTIALS: ecs.Secret.fromSecretsManager(dbCredential),
       },
     })
