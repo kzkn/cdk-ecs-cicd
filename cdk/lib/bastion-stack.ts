6a640f49e3e984bb0f232bfdc6079854e404dbf5
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2023-02-02 21:44:39 +0900

    fix policy

--- cdk/lib/bastion-stack.ts
+++ cdk/lib/bastion-stack.ts
@@ -20,6 +20,8 @@ export class BastionStack extends cdk.Stack {
       cpu: 256,
       memoryLimitMiB: 512,
     })
+    // An error occurred (AccessDeniedException) when calling the CreateActivation operation: User: arn:aws:sts::826145371799:assumed-role/DevBastionStack-TaskDefTaskRole1EDB4A67-1IQH9ORNNV2MZ/a4ef06e8bdf44456872f2bd4a1cd044a is not authorized to perform: ssm:CreateActivation on resource: arn:aws:iam::826145371799:role/DevBastionStack-SsmServiceRole30DCFE5E-8I2PFHYH4AYW because no identity-based policy allows the ssm:CreateActivation action
+    // Attach Managed Policy: AmazonSSMFullAccess
     taskDef.addToTaskRolePolicy(new iam.PolicyStatement({
       actions: ['iam:PassRole'],
       resources: ['*'],
@@ -27,10 +29,31 @@ export class BastionStack extends cdk.Stack {
         StringEquals: { 'iam:PassedToService': 'ssm.amazonaws.com' }
       }
     }))
+    taskDef.taskRole.addManagedPolicy({ managedPolicyArn: 'arn:aws:iam::aws:policy/AmazonSSMFullAccess' })
 
     const dbCredential = props.dbInstance.secret!
+    // aws iam attach-role-policy \
+    // --role-name SSMServiceRole \
+    // --policy-arn arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore  
+// {
+//     "Version": "2012-10-17",
+//     "Statement": [
+//         {
+//             "Effect": "Allow",
+//             "Action": [
+//                 "ssm:CreateActivation"
+//             ],
+//             "Resource": "*"
+//         }
+//     ]
+// }
+
+    // Attach Managed Policy: AmazonSSMManagedInstanceCore
     const ssmServiceRole = new iam.Role(this, 'SsmServiceRole', {
-      assumedBy: new iam.ServicePrincipal('ssm.amazonaws.com')
+      assumedBy: new iam.ServicePrincipal('ssm.amazonaws.com'),
+      managedPolicies: [
+        { managedPolicyArn: 'arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore' },
+      ]
     })
 
     // TODO: bastion タスクに public ip を assign しないと起動しないのを改善したい
